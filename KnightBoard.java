91e8a1442f6637e689a26c71168d9487eb8a1da9
Kevin Chen <kchen01@stuy.edu>
2019-02-14 11:13:58 -0500

    class work of 2/14/19 did addKnight removeKnight and toStirng

--- KnightBoard.java
+++ KnightBoard.java
@@ -7,84 +7,155 @@ public class KnightBoard {
       }
       for (int i = 0; i < board.length; i++) {
         for ( int j = 0; j < board[0].length; j++) {
+          if (board[i][j] == 0) {
+            output += "_ ";
+          }
+          else {
           output += board[i][j] + " ";
         }
+      }
         output += "\n";
       }
-      return outpuhgt;
+      return output;
     }
     public KnightBoard(int startingRows,int startingCols) {
       board = new int[startingRows][startingCols];
     }
     public boolean addKnight(int r, int c, int count, int position) {
-      if (postion == 0) {
-        if (c - 2 > -1 && r - 1 > -1 && board[r][c] == 0) {
+      if (position == 0) {
+        if (r - 2 > -1 && c - 1 > -1 && board[r][c] == 0) {
           board[r][c] = count;
           return true;
         }
-        else {
-          return false;
         }
-      if (postion == 1) {
-        if (c - 2 > -1 && r - 1 > -1 && board[r][c] == 0) {
+      if (position == 1) {
+        if (r - 2 > -1 && c + 1 < board[0].length && board[r][c] == 0) {
           board[r][c] = count;
           return true;
         }
-        else {
-          return false;
-        }
-      if (postion == 2) {
-        if (c - 1 > -1 && r - 2 > -1 && board[r][c] == 0) {
+      }
+      if (position == 2) {
+        if (r - 1 > -1 && c + 2 < board[0].length && board[r][c] == 0) {
           board[r][c] = count;
           return true;
         }
-          else {
-            return false;
+      }
+      if (position == 3) {
+        if (r + 1 < board.length && c + 2 < board[0].length && board[r][c] == 0) {
+          board[r][c] = count;
+          return true;
+        }
           }
-      if (postion == 3) {
-        if (c + 1 > board.length && r - 2 > -1 && board[r][c] == 0) {
+      if (position == 4) {
+        if (r + 2 < board.length && c + 1 < board[0].length && board[r][c] == 0) {
           board[r][c] = count;
           return true;
-        }
-          else {
-            return false;
+            }
+            }
+      if (position == 5) {
+        if (r + 2 < board.length && c - 1 > -1 && board[r][c] == 0) {
+          board[r][c] = count;
+          return true;
+            }
+              }
+      if (position == 6) {
+        if (r + 1 < board.length && c - 2 > -1 && board[r][c] == 0) {
+          board[r][c] = count;
+          return true;
           }
-      if (postion == 4) {
-        if (c - 2 > -1 && r - 1 > -1 && board[r][c] == 0) {
-          board[r][c] = count;
-          return true;
         }
-          else {
-          return false;
+        if (position == 7) {
+            if (r -1  > -1 && c - 2 > -1 && board[r][c] == 0) {
+              board[r][c] = count;
+              return true;
+          }
         }
-      if (postion == 5) {
-        if (c - 2 > -1 && r - 1 > -1 && board[r][c] == 0) {
-          board[r][c] = count;
-          return true;
-        }
-          else {
             return false;
         }
+      public boolean remvoeKnight(int r, int c, int count, int position) {
+        if (position == 0) {
+          if (r - 2 > -1 && c - 1 > -1 && board[r][c] != 0) {
+            board[r][c] = 0;
+            return true;
+          }
+          }
+        if (position == 1) {
+          if (r - 2 > -1 && c + 1 < board[0].length && board[r][c] != 0) {
+            board[r][c] = 0;
+            return true;
+          }
+        }
+        if (position == 2) {
+          if (r - 1 > -1 && c + 2 < board[0].length && board[r][c] != 0) {
+            board[r][c] = 0;
+            return true;
+          }
+        }
+        if (position == 3) {
+          if (r + 1 < board.length && c + 2 < board[0].length && board[r][c] != 0) {
+            board[r][c] = 0;
+            return true;
+          }
+            }
+        if (position == 4) {
+          if (r + 2 < board.length && c + 1 < board[0].length && board[r][c] != 0) {
+            board[r][c] = 0;
+            return true;
+              }
+              }
+        if (position == 5) {
+          if (r + 2 < board.length && c - 1 > -1 && board[r][c] != 0) {
+            board[r][c] = 0;
+            return true;
+              }
+                }
+        if (position == 6) {
+          if (r + 1 < board.length && c - 2 > -1 && board[r][c] != 0) {
+            board[r][c] = 0;
+            return true;
+            }
+          }
+          if (position == 7) {
+              if (r -1  > -1 && c - 2 > -1 && board[r][c] != 0) {
+                board[r][c] = 0;
+                return true;
+            }
+          }
+              return false;
+          }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
       }
-      if (postion == 6) {
-        if (c - 2 > -1 && r - 1 > -1 && board[r][c] == 0) {
-          board[r][c] = count;
-          return true;
-        }
-          else {
-            return false;
-        }
-      }
-      if (postion == 7) {
-        if (c - 2 > -1 && r - 1 > -1 && board[r][c] == 0) {
-          board[r][c] = count;
-          return true;
-        }
-          else {
-            return false;
-        }
-      }
-    }
-  }
-}
-}}}}
---
+++ test.java
@@ -0,0 +1,12 @@
+public class test{
+
+  public static void main(String[] args) {
+    KnightBoard testeryi = new KnightBoard(5,5);
+    System.out.println(testeryi);
+
+
+  }
+
+
+
+}
