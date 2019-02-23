dbc61529868bf3fad8969dcf48947c0c72ee1481
Kevin Chen <kchen01@stuy.edu>
2019-02-22 22:00:31 -0500

    tiny testing driver

--- test.java
+++ test.java
@@ -1,9 +1,8 @@
 public class test{
 
   public static void main(String[] args) {
-    KnightBoard testeryi = new KnightBoard(5,5);
-    System.out.println(testeryi);
-
+    KnightBoard testeryi = new KnightBoard(6,6);
+    System.out.println(testeryi.count());
 
   }
 
