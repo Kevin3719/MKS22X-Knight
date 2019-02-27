7be3c7fa2be63aa5897f3df4fe6a657e4ea75c1a
Kevin Chen <kchen01@stuy.edu>
2019-02-26 22:22:59 -0500

    added comments

--- KnightBoard.java
+++ KnightBoard.java
@@ -1,6 +1,7 @@
 public class KnightBoard {
     public int[][] board;
     public int[][] optimizer;
+    //simple to String method
     public String toString() {
       String output = "";
       if (board.length == 0) {
@@ -22,6 +23,7 @@ public class KnightBoard {
       }
       return output;
     }
+    //only for debugging purposes
     public String printchart() {
       String output = "";
       if (optimizer.length == 0) {
@@ -40,6 +42,7 @@ public class KnightBoard {
       }
       return output;
     }
+    //only to construct the optimizer chart
     public void makechart() {
       for (int i = 0; i < optimizer.length;i++) {
         if (i == 0 || i == optimizer.length - 1) {
@@ -77,6 +80,7 @@ public class KnightBoard {
         }
       }
     }
+    //throws exceptions when any inserted number is less than 0
     public KnightBoard(int startingRows,int startingCols) {
       if (startingRows < 0 || startingCols < 0) {
         throw new IllegalArgumentException();
@@ -85,13 +89,26 @@ public class KnightBoard {
       optimizer = new int[startingRows][startingCols];
       makechart();
     }
+    //shortens code function that checks if it is within bounds
     public boolean isGood(int r, int c) {
       return  r >= 0 && c >= 0 && r < board.length && c < board[0].length && board[r][c] == 0;
     }
+    //throws exceptions when inserted numbers are too high or low. Also throws exceptions when the inserted chart isnt empty.
     public int countSolutions(int row, int col) {
+      if (row < 0 || col < 0 || row > board.length || col > board[0].length) {
+        throw new IllegalArgumentException();
+      }
+      for(int i=0;i<board.length;i++){
+        for(int j=0;j<board[0].length;j++){
+          if(board[i][j]!=0){
+            throw new IllegalStateException();
+          }
+        }
+      }
       board[row][col] = 1;
       return helpcount(row,col,0,1);
     }
+
     public int helpcount(int row, int col, int count, int counter){
       int addrow = 0;
       int addcol = 0;
@@ -139,7 +156,8 @@ public class KnightBoard {
         }
         return count;
       }
-
+      // optimized solve. Uses the idea talked about in class.
+      //throws exceptions when inserted numbers are too high or low. Also throws exceptions when the inserted chart isnt empty.
       public boolean solve(int row, int col) {
         if (row < 0 || col < 0 || row > board.length || col > board[0].length) {
           throw new IllegalArgumentException();
@@ -158,6 +176,7 @@ public class KnightBoard {
         board[row][col] = 0;
         return false;
       }
+      // uses the idea we talked about in class
       public boolean help(int row, int col, int count, int counter){
         if (count > 0) {
           return true;
@@ -210,6 +229,7 @@ public class KnightBoard {
             movescount[i] = 0;
           }
         }
+        // very ugly selectionSort method.
         int smallest = movescount[0];
         int index = 0;
         for (int i = 0; i < 8; i++) {
